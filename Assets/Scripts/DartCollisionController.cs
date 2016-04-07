8538055fd6aa8676302bc849fcb7ce71d2b828b7
Spenser Solys <solysspenser@gmail.com>
2016-04-06 23:24:05 -0400

    Fixed water
    Added dart scripts

---
+++ Assets/Scripts/DartCollisionController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DartCollisionController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+
+		if(col.gameObject.tag == "Animal") {
+			//col.GetComponent<AnimalController>().hitWithTranq();
+		}
+		Destroy(gameObject);
+
+	}
+}
